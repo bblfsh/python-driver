84b06e1c42f4b610f3d7ee56bd387ab4576d1152
Juanjo Alvarez <juanjux@gmail.com>
2017-02-19 03:25:55 +0100

    rationalized error control

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ MAINTAINER source{d}
 
 RUN apt update
 RUN apt install -y python2.7 python2.7-dev python-pip python3 python3-dev \
-    python3-six python3-pip
+    python3-six python3-pip git
 RUN pip3 install --upgrade pip
 RUN pip3 install msgpack-python
 RUN pip3 install six
--- bin/pyparser.py
+++ bin/pyparser.py
@@ -1,18 +1,53 @@
 import sys
-import time
 import msgpack
 import pydetector.detector as detector
-from pprint import pprint
 from traceback import format_exc
 
 # TODO: modify the AST adding comments, use tokenizer?
 # TODO: detect the end of the stdin in a less kludgy way
-# TODO: change the asserts for better error control
 # TODO: command line option to export JSON or just pprint
 # the output for debugging (to sys.stdout, unbuffered)
 
+__version__ = '1.0'
+
+class RequestCheckException(Exception): pass
+
+def check_input_request(request, errors):
+
+    with_error = False
+    if b'content' not in request or not request[b'content']:
+        errors.append('Bad input message, missing content')
+        with_error = True
+
+    if b'language' in request and request[b'language'].lower() != b"python":
+        errors.append('Bad language requested for the Python driver: "%s"'
+                      % request[b'language'].decode('utf-8'))
+        with_error = True
+
+    if b'language_version' in request and \
+            request[b'language_version'][0] not in (b'1', b'2', b'3'):
+        errors.append('Bad language version requested, Python driver only '
+                      'supports versions 1, 2 and 3')
+        with_error = True
+
+    if with_error:
+        raise RequestCheckException()
+
+
+def return_error(status='error', errors=''):
+    outdict = {
+            'status': status,
+            'errors': errors[0] if isinstance(errors, str) else errors,
+            'driver': 'python23:%s' % __version__,
+    }
+    sys.stdout.buffer.write(msgpack.dumps(outdict))
+    sys.stdout.flush()
+
+
 def main():
     while True:
+        errors = []
+
         try:
             try:
                 request = msgpack.load(sys.stdin.buffer)
@@ -21,37 +56,26 @@ def main():
                 # FIXME
                 exit()
 
-            assert b'content' in request and request[b'content']
-            if b'language' in request:
-                assert request['language'].lower() == 'python'
-            if b'language_version' in request:
-                assert str(request[b'language_version'])[0] in ('1', '2', '3')
+            check_input_request(request, errors)
 
             code = request[b'content']
             status = 'ok'
-            errors = []
             version = 0
             ast = ''
 
-            try:
-                resdict = detector.detect(codestr = code.decode('utf-8'), stop_on_ok_ast=True)
-            except:
-                status = 'error'
-                errors.append('Exception trying to get the AST: {}'.format(format_exc()))
-            else:
-                codeinfo = resdict['<code_string>']
-                version = codeinfo['version']
-                if version in (3, 6):
-                    ast = codeinfo['py3ast']
-                elif version == 2:
-                    ast = codeinfo['py2ast']
-                else:
-                    status = 'error'
-                    errors.append('Could not determine Python version')
+            resdict = detector.detect(codestr = code.decode('utf-8'), stop_on_ok_ast=True)
+            codeinfo = resdict['<code_string>']
+            version = codeinfo['version']
 
-                if not ast:
-                    status = 'error'
-                    errors.append('Empty AST generated')
+            if version in (3, 6):
+                ast = codeinfo['py3ast']
+            elif version == 2:
+                ast = codeinfo['py2ast']
+            else:
+                raise Exception('Could not determine Python version')
+
+            if not ast:
+                raise Exception('Empty AST generated')
 
             outdict = {
                 'status': status,
@@ -64,16 +88,8 @@ def main():
             sys.stdout.buffer.write(msgpack.dumps(outdict))
             sys.stdout.flush()
 
-        except Exception as e:
-            # FIXME: log the error
-            outdict = {
-                'status': 'fatal',
-                'driver': 'python23:1.0',
-                'errors': [format_exc()],
-            }
-            sys.stdout.buffer.write(msgpack.dumps(outdict))
-            sys.stdout.flush()
-            time.sleep(0.3)
+        except:
+            return_error(status='fatal', errors=[format_exc()])
 
 
 if __name__ == '__main__':
