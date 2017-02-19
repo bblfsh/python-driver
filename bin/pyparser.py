8951caa03a10015f93c8b5c147e3704ae9e4e1e1
Juanjo Alvarez <juanjux@gmail.com>
2017-02-19 11:25:26 +0100

    Kosher way to stream-read from stdin and debug helper option to print output as string
    docstrings and some more comments

--- bin/pyparser.py
+++ bin/pyparser.py
@@ -1,30 +1,43 @@
 import sys
 import msgpack
 import pydetector.detector as detector
+from pprint import pprint
 from traceback import format_exc
 
 # TODO: modify the AST adding comments, use tokenizer?
-# TODO: detect the end of the stdin in a less kludgy way
-# TODO: command line option to export JSON or just pprint
-# the output for debugging (to sys.stdout, unbuffered)
+# and use dependency injection to pass a custom ast module that extract
+# comments to pydetector
 
 __version__ = '1.0'
 
 class RequestCheckException(Exception): pass
 
 def check_input_request(request, errors):
+    """
+    Check the incoming request package and validate that the 'content' and
+    'language' keys are not missing and that 'language' and 'language_version'
+    are the right ones for this driver.
+
+    Args:
+        request (bytes, messagepack): The incoming request in messagepack format
+            as bytes.
+
+        errors (List[str]): the list of errors. New errors will be added to it in place.
+    Raises:
+        RequestCheckException if the request failed to validate.
+    """
 
     with_error = False
     if b'content' not in request or not request[b'content']:
         errors.append('Bad input message, missing content')
         with_error = True
 
-    if b'language' in request and request[b'language'].lower() != b"python":
+    if b'language' in request and request[b'language'].lower() != b'python':
         errors.append('Bad language requested for the Python driver: "%s"'
                       % request[b'language'].decode('utf-8'))
         with_error = True
 
-    if b'language_version' in request and \
+    if 'language_version' in request and \
             request[b'language_version'][0] not in (b'1', b'2', b'3'):
         errors.append('Bad language version requested, Python driver only '
                       'supports versions 1, 2 and 3')
@@ -45,51 +58,71 @@ def return_error(status='error', errors=''):
 
 
 def main():
+    """
+    Main loop. It will read msgpack incoming requests from stdin using
+    the msgpack lib stream unpacker. In case of error it will generate
+    an error reply, also in msgpack format. The replies will be printed
+    in stdout using bytes.
+
+    If you pass the --print command line parameter the replies will be
+    printed using pprint without wrapping them in the msgpack format which
+    can be handy when debugging.
+    """
+    # TODO: check what errors are fatal and what to do with them
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--print':
+        outformat = 'print'
+    else:
+        outformat = 'msgpack'
+
     while True:
         errors = []
 
         try:
-            try:
-                request = msgpack.load(sys.stdin.buffer)
-            except msgpack.exceptions.UnpackValueError:
-                # finished kludge
-                # FIXME
-                exit()
+            for request in msgpack.Unpacker(sys.stdin.buffer):
 
-            check_input_request(request, errors)
+                check_input_request(request, errors)
+                code     = request[b'content']
+                # We want the code detection to be fast and we prefer Python3 AST so using
+                # the stop_on_ok_ast will avoid running a Python2 subprocess to check the
+                # AST with Python2 if the Python3 version (which is a better AST anyway) worked
+                resdict  = detector.detect(codestr=code, stop_on_ok_ast=True)
+                codeinfo = resdict['<code_string>']
+                version  = codeinfo['version']
 
-            code = request[b'content']
-            status = 'ok'
-            version = 0
-            ast = ''
+                if version in (3, 6):
+                    ast = codeinfo['py3ast']
+                elif version in (1, 2):
+                    ast = codeinfo['py2ast']
+                else:
+                    raise Exception('Could not determine Python version')
 
-            resdict = detector.detect(codestr = code.decode('utf-8'), stop_on_ok_ast=True)
-            codeinfo = resdict['<code_string>']
-            version = codeinfo['version']
+                if not ast:
+                    raise Exception('Empty AST generated')
 
-            if version in (3, 6):
-                ast = codeinfo['py3ast']
-            elif version == 2:
-                ast = codeinfo['py2ast']
-            else:
-                raise Exception('Could not determine Python version')
+                outdict = {
+                    'status': 'ok',
+                    'errors': errors,
+                    'language': 'python',
+                    'language_version': version,
+                    'driver': 'python23:1.0',
+                    'ast': ast,
+                }
 
-            if not ast:
-                raise Exception('Empty AST generated')
+                if outformat == 'print':
+                    pprint(outdict)
+                else:
+                    # sys.stdout.buffer is the byte stream and since msgpack generates
+                    # byte-typed data on Python3 is the one we need to use
+                    sys.stdout.buffer.write(msgpack.dumps(outdict))
 
-            outdict = {
-                'status': status,
-                'errors': errors,
-                'language': 'python',
-                'language_version': version,
-                'driver': 'python23:1.0',
-                'ast': ast,
-            }
-            sys.stdout.buffer.write(msgpack.dumps(outdict))
-            sys.stdout.flush()
+                sys.stdout.flush()
+
+        except KeyboardInterrupt:
+            exit(1)
 
         except:
-            return_error(status='fatal', errors=[format_exc()])
+            return_error(status='error', errors=[format_exc()])
 
 
 if __name__ == '__main__':
--- bin/sendmsg.py
+++ bin/sendmsg.py
@@ -1,8 +1,5 @@
 import msgpack
 import sys
-from subprocess import *
-
-# p = Popen(["python3", "pyparser.py"], stdin=PIPE, stdout=PIPE)
 
 files = sys.argv[1:]
 for f in files:
@@ -11,6 +8,4 @@ for f in files:
         'content': open(f).read()
     }
     msg = msgpack.packb(d)
-    # p.stdin.b
-    # print(msg.decode(errors='ignore'))
     sys.stdout.buffer.write(msg)
