0d2364a66d16c08fb2c7cc98a4152e82adc53255
Juanjo Alvarez <juanjux@gmail.com>
2017-03-28 17:14:16 +0200

    new pydetector.astexport version should ensure line numbers for most nodes

--- native/python_package/setup.py
+++ native/python_package/setup.py
@@ -34,7 +34,7 @@ setup(
     },
     install_requires=[
         "msgpack-python==0.4.8",
-        "pydetector==0.4.5"
+        "pydetector==0.4.6"
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
