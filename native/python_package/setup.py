e43bdb8a04977a5e66fce753745953bd9eb631cd
Juanjo Alvarez <juanjo@sourced.tech>
2017-09-21 14:26:20 +0200

    Bump pydetector version

--- native/python_package/setup.py
+++ native/python_package/setup.py
@@ -31,7 +31,7 @@ setup(
         ]
     },
     install_requires=[
-        "pydetector-bblfsh==0.14.1"
+        "pydetector-bblfsh==0.14.2"
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
