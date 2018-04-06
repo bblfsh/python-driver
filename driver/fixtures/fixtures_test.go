8bca17416c5d1f2cf1ef6628ce692046dbc9aab1
Juanjo Alvarez <juanjo@sourced.tech>
2018-04-06 16:19:55 +0200

    Migration to the new DSL.
    Updated python skeleton driver to the latest changes
    
    Completed covering all the node types, added some missing tokens
    
    Fixed the problem with comment annotations
    
    Remove some outdates FIXME reminders and other unneeded stuff
    
    Address some issues from review
    
    Add the Unannotated transformer. Fix some unannotated nodes
    
    Fix import and alias tokens, add more synth tokens.
    
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

---
+++ driver/fixtures/fixtures_test.go
@@ -0,0 +1,34 @@
+package fixtures
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/bblfsh/python-driver/driver/normalizer"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver"
+	"gopkg.in/bblfsh/sdk.v1/sdk/driver/fixtures"
+)
+
+const projectRoot = "../../"
+
+var Suite = &fixtures.Suite{
+	Lang: "python",
+	Ext:  ".py",
+	Path: filepath.Join(projectRoot, fixtures.Dir),
+	NewDriver: func() driver.BaseDriver {
+		return driver.NewExecDriverAt(filepath.Join(projectRoot, "build/bin/native"))
+	},
+	Transforms: driver.Transforms{
+		Native: normalizer.Native,
+		Code:   normalizer.Code,
+	},
+	BenchName: "issue_server101",
+}
+
+func TestPythonDriver(t *testing.T) {
+	Suite.RunTests(t)
+}
+
+func BenchmarkPythonDriver(b *testing.B) {
+	Suite.RunBenchmarks(b)
+}
--- driver/main.go
+++ driver/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	_ "github.com/bblfsh/python-driver/driver/impl"
 	"github.com/bblfsh/python-driver/driver/normalizer"
 
 	"gopkg.in/bblfsh/sdk.v1/sdk/driver"
 )
 
 func main() {
-	driver.Run(normalizer.ToNode, normalizer.Transformers)
+	driver.Run(driver.Transforms{
+		Native: normalizer.Native,
+		Code:   normalizer.Code,
+	})
 }
