f6ebaf3bfe27a99b6b20cef96ec803376c3905ab
Denys Smirnov <denys@sourced.tech>
2018-11-02 19:39:49 +0200

    enable language namespace in semantic uast
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/normalizer/transforms.go
+++ driver/normalizer/transforms.go
@@ -3,6 +3,7 @@ package normalizer
 import "gopkg.in/bblfsh/sdk.v2/driver"
 
 var Transforms = driver.Transforms{
+	Namespace:   "python",
 	Preprocess:  Preprocess,
 	Normalize:   Normalize,
 	Annotations: Native,
